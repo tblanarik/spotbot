e61bdc3b2bac92cc2857d02b5fae642059d6f4aa
Trevor Blanarik <tblanarik@github.com>
2024-09-21 05:04:04 +0000

    creating the spot deeplink function and fixing test casing

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import function_app
 class TestSpotBot(unittest.TestCase):
 
     def test_function_app_basic(self):
-        req_body = {"fullCallsign": "KI7HSG", "source": "POTA", "frequency": "14.074", "mode": "FT8", "wwffRef":"US-0052"}
+        req_body = {"fullCallsign": "KI7HSG", "source": "pota", "frequency": "14.074", "mode": "FT8", "wwffRef":"US-0052"}
         content = function_app.create_content(req_body)
-        expected = {'content': 'KI7HSG | POTA | freq: 14.074 | mode: FT8 | loc: US-0052'}
+        expected = {'content': 'KI7HSG | pota | freq: 14.074 | mode: FT8 | loc: US-0052'}
         self.assertDictEqual(content, expected)
 
     def test_function_app(self):
