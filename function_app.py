22bcacb35cb9fbde9c59cf00caa464f8e5c9209c
Trevor Blanarik <tblanarik@github.com>
2024-09-28 21:03:47 +0000

    adds new cleanup function and adds exception handling

--- function_app.py
+++ function_app.py
@@ -1,9 +1,20 @@
+import logging
 import azure.functions as func
 import spotbot as sb
+import cleanup
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="spotbot", methods=[func.HttpMethod.POST])
 def spotbot(req: func.HttpRequest) -> func.HttpResponse:
-    sb.run(req)
-    return func.HttpResponse(status_code=202)
+    try:
+        sb.run(req)
+    except Exception as _excpt:
+        logging.error(f"Exception occurred: {_excpt}")
+        return func.HttpResponse(body=f"Exception occurred: {_excpt}", status_code=500)
+    else:
+        return func.HttpResponse(status_code=202)
+
+@app.route(route="manualcleanup", methods=[func.HttpMethod.POST])
+def manual_cleanup(req: func.HttpRequest) -> func.HttpResponse:
+    cleanup.cleanup()
