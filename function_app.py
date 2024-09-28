7447ced60eee8d9e75fcd43c883338b54564d891
Trevor Blanarik <tblanarik@github.com>
2024-09-28 11:13:19 -0700

    Revert "Error handling and stubbing out table cleanup methods"

--- function_app.py
+++ function_app.py
@@ -1,27 +1,9 @@
-import logging
 import azure.functions as func
 import spotbot as sb
-import cleanup
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="spotbot", methods=[func.HttpMethod.POST])
 def spotbot(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        sb.run(req)
-    except Exception as _excpt:
-        logging.error(f"Exception occurred: {_excpt}")
-        return func.HttpResponse(body=f"Exception occurred: {_excpt}", status_code=500)
-    else:
-        return func.HttpResponse(status_code=202)
-
-@app.function_name(name="tablecleanup")
-@app.schedule(schedule="0 8 * * *",
-              arg_name="tablecleanup",
-              run_on_startup=False)
-def table_cleanup(timer: func.TimerRequest) -> None:
-    cleanup.cleanup()
-
-@app.route(route="manualcleanup", methods=[func.HttpMethod.POST])
-def manual_cleanup(req: func.HttpRequest) -> func.HttpResponse:
-    cleanup.cleanup()
+    sb.run(req)
+    return func.HttpResponse(status_code=202)
