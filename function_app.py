b757c53437e28970987a5ee4cb532f25cf64c530
Trevor Blanarik <tblanarik@github.com>
2024-09-28 17:47:34 +0000

    wrap in a try/catch and return 500 on exception

--- function_app.py
+++ function_app.py
@@ -1,9 +1,15 @@
 import azure.functions as func
 import spotbot as sb
+import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="spotbot", methods=[func.HttpMethod.POST])
 def spotbot(req: func.HttpRequest) -> func.HttpResponse:
-    sb.run(req)
-    return func.HttpResponse(status_code=202)
+    try:
+        sb.run(req)
+    except Exception as _excpt:
+        logging.error(f"Exception occurred: {_excpt}")
+        return func.HttpResponse(body=f"Exception occurred: {_excpt}", status_code=500)
+    else:
+        return func.HttpResponse(status_code=202)
