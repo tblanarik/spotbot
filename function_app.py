da043f0cbf81901739acf6931a97d2d8f5a12538
Trevor Blanarik <tblanarik@github.com>
2024-09-28 18:02:23 +0000

    stubbing out table cleanup

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import spotbot as sb
+import cleanup
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
@@ -13,3 +14,14 @@ def spotbot(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(body=f"Exception occurred: {_excpt}", status_code=500)
     else:
         return func.HttpResponse(status_code=202)
+
+@app.function_name(name="tablecleanup")
+@app.schedule(schedule="0 8 * * *",
+              arg_name="tablecleanup",
+              run_on_startup=False)
+def table_cleanup(timer: func.TimerRequest) -> None:
+    cleanup.cleanup()
+
+@app.route(route="manualcleanup", methods=[func.HttpMethod.POST])
+def manual_cleanup(req: func.HttpRequest) -> func.HttpResponse:
+    cleanup.cleanup()
