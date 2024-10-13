908449dbbfa2bc9aada319ffe3b464ab86945589
Trevor Blanarik <tblanarik@github.com>
2024-10-13 04:22:07 +0000

    keeps a running log of messages, now with timestamps

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 import unittest
 import spotbot
+from datetime import datetime
+from pytz import timezone
 
 class TestSpotBot(unittest.TestCase):
 
     def test_function_app_basic(self):
+        dd = datetime.strptime("2024-10-13T01:05:03", "%Y-%m-%dT%H:%M:%S")
         req_body = {"callsign":"KI7HSG", "source": "pota", "frequency": "14.074", "mode": "FT8", "wwffRef":"US-0052"}
-        content = spotbot.create_content(req_body)
-        expected = {'content': 'KI7HSG | [pota](https://api.pota.app/spot/comments/KI7HSG/US-0052) | freq: 14.074 | mode: FT8 | loc: US-0052', 'flags': 4}
+        content = spotbot.create_content(req_body, dd)
+        expected = {'content': 'KI7HSG | [pota](https://api.pota.app/spot/comments/KI7HSG/US-0052) | freq: 14.074 | mode: FT8 | loc: US-0052 | 01:05', 'flags': 4}
         self.assertDictEqual(content, expected)
 
     def test_function_app(self):
+        dd = datetime.strptime("2024-10-13T01:05:03", "%Y-%m-%dT%H:%M:%S")
         req_body = {"callsign":"KI7HSG", "source": "sotawatch", "frequency": "14.074", "mode": "FT8", "summitRef": "ABCD"}
-        content = spotbot.create_content(req_body)
-        expected = {'content': 'KI7HSG | [sotawatch](https://sotl.as/activators/KI7HSG) | freq: 14.074 | mode: FT8 | loc: ABCD', 'flags': 4}
+        content = spotbot.create_content(req_body, dd)
+        expected = {'content': 'KI7HSG | [sotawatch](https://sotl.as/activators/KI7HSG) | freq: 14.074 | mode: FT8 | loc: ABCD | 01:05', 'flags': 4}
         self.assertDictEqual(content, expected)
 
 if __name__ == '__main__':
