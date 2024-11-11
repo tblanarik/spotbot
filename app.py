261c1992fce25458d690bf9b22d00b2b3a65299a
Trevor Blanarik <tblanarik@github.com>
2024-11-11 07:02:37 +0000

    flasking this app

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-import uuid
-import os
+from flask import Flask, request, make_response
+import logging
+import spotbot as sb
+import tables
+import discord_http
 app = Flask(__name__)
 
 @app.route(f'/message')
 def run():
-    return f'Hello world'
+    try:
+        sb.SpotBot(request, tables.HamAlertTable(), discord_http.DiscordHttp()).process()
+    except Exception as _excpt:
+        logging.error(f"Exception occurred: {_excpt}")
+        return make_response(f"Exception occurred: {_excpt}", 500)
+    else:
+        return make_response("Accepted", 202)
 
 if __name__ == "__main__":
     app.run()
