981f03e9897a70aa20d4f987da4749c9d4068209
Jay Vana <jaysvana@gmail.com>
2025-11-13 16:17:47 -0800

    Fix column field

--- init.sql
+++ init.sql
@@ -2,6 +2,6 @@
 CREATE TABLE IF NOT EXISTS spots (
     callsign VARCHAR(20) PRIMARY KEY,
     message_id VARCHAR(100) NOT NULL,
-    utctimestamp DATETIME NOT NULL,
+    utctimestamp TIMESTAMP NOT NULL,
     INDEX idx_utctimestamp (utctimestamp)
 );
