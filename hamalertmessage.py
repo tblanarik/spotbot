1ace29903f377b41d8e0061326b11cdb63adc664
Trevor Blanarik <tblanarik@gmail.com>
2025-11-22 22:05:01 -0800

    Add more sources

--- hamalertmessage.py
+++ hamalertmessage.py
@@ -17,8 +17,14 @@ class HamAlertMessage:
                 return f"[{self.source}](https://sotl.as/activators/{self.callsign})"
             case "pota":
                 return f"[{self.source}](https://pota.app/#/park/{self.wwffRef})"
+            case "dxwatch":
+                return f"[{self.source}](https://dxwatch.com/dxsd1/dxsd1.php?f=0&t=dx&c={self.callsign})"
+            case "pskreporter":
+                return f"[{self.source}](https://pskreporter.info/pskmap?callsign={self.callsign}&search=Find)"
+            case "rbn":
+                return f"[{self.source}](https://www.reversebeacon.net/main.php?rows=10&max_age=10,hours&spotted_call={self.callsign}&hide=distance_km)"
             case _:
-                return self.source
+                return f"[{self.source}](https://dxwatch.com/dxsd1/dxsd1.php?f=0&t=dx&c={self.callsign})"
 
     def __str__(self):
         return f'{self.received_time_pt.strftime("%H:%M")} | [{self.callsign}](https://www.qrz.com/db/{self.callsign}) | {self.spot_deeplink()} | freq: {self.frequency} | mode: {self.mode} | loc: {self.summitRef}{self.wwffRef}'
